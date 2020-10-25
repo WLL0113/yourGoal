0e385e1b86fec892f54f954706a802c67011569a
wll <wll0113@icloud.com>
2020-10-25 15:27:48 +0800

    init

---
+++ src/pages/Home/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>职业选择</h1>
+    <div class="content">
+      <p>《中华人民共和国职业分类大典》将我国职业归为8个大类，66个中类，413个小类，1838个细类(职业)。</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+</style>
